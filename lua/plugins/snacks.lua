987675a845c28e3b51e01972aa221ac1807a38c0
Ivan Gurkov <anhyrys@gmail.com>
2025-05-20 17:11:28 +0300

    linux name change

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -26,7 +26,7 @@ return {
           -- cmd = 'chafa "C:\\Users\\Ivan\\Desktop\\PAPKA\\8bitpixelartvista.jpg" --format symbols --symbols vhalf --size 55x10 --stretch',
           cmd = (vim.loop.os_uname().sysname == "Windows_NT")
               and 'chafa "C:\\Users\\Ivan\\Desktop\\PAPKA\\8bitpixelartvista.jpg" --format symbols --symbols vhalf --size 55x10 --stretch' -- Windows command
-            or 'chafa "/home/zovutvanya/8bitpixelartvista.jpg" --format symbols --symbols vhalf --size 55x10 --stretch', -- Linux/Unix command
+            or 'chafa "/home/penpen/8bitpixelartvista.jpg" --format symbols --symbols vhalf --size 55x10 --stretch', -- Linux/Unix command
           -- cmd = 'pat "C:\\Users\\Ivan\\Desktop\\PAPKA\\8bitpixelartvista55x15.jpg"',
           -- cmd = "ascii-image-converter 'C:\\Users\\Ivan\\Desktop\\PAPKA\\8bitpixelartvista.jpg' -C -c -d 55,15",
           height = 10,
