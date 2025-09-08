6124af7c7e48cc77bf7c9bc29943c0ede10e86ee
Ivan Gurkov <ivangurkoff@gmail.com>
2025-09-08 11:27:13 +0700

    move zshn linting lower into the config

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -19,23 +19,24 @@ return {
           return not ctx.filename:match("%.env")
         end,
       },
-      zshn = {
-        name = "zshn",
-        cmd = "zsh",
-        stdin = false,
-        append_fname = true,
-        args = { "-n" }, -- syntax check only
-        stream = "stderr",
-        ignore_exitcode = true,
-        parser = require("lint.parser").from_errorformat(
-          "%f:%l: %m",
-          { source = "zsh -n", severity = vim.diagnostic.severity.ERROR }
-        ),
-      },
     },
   },
   config = function(_, opts)
     local lint = require("lint")
+
+    lint.linters.zshn = {
+      name = "zshn",
+      cmd = "zsh",
+      stdin = false,
+      append_fname = true,
+      args = { "-n" }, -- syntax check only
+      stream = "stderr",
+      ignore_exitcode = true,
+      parser = require("lint.parser").from_errorformat(
+        "%f:%l: %m",
+        { source = "zsh -n", severity = vim.diagnostic.severity.ERROR }
+      ),
+    }
 
     -- Prepend --shell=bash to shellcheck args
     table.insert(lint.linters.shellcheck.args, 1, "--shell=bash")
